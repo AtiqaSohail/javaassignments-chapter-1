166da0b1baf4307f941be8caf33fbf49b38b408f
AtiqaSohail <164095247+AtiqaSohail@users.noreply.github.com>
2024-11-15 16:44:42 -0800

    chap 1

---
+++ app.js
@@ -0,0 +1,36 @@
+// Question # 01 : Write a script to greet your website visitor using JS alert
+// box.
+
+alert("Welcome to our website! We hope you have a great experience.");
+
+// Question # 02 : Write a script to display following message on your web
+// page:
+
+alert("Error! Please Enter a Valid Password");
+
+// Question # 03 : Write a script to display following message on your web
+// page: (Hint : Use line break)
+
+alert("Welcome to JS Land... \n"  + "Happy Coding!");
+
+// Question # 04 : 4. Write a script to display following messages in sequence: 
+
+
+alert("Welcome to JS Land...");
+alert("Happy Coding!\n(Prevent this page from creating additional dialogs.)");
+
+// Question # 05 : Generate the following message through browser’s
+// developer console:
+ 
+alert("Hello... I can run JS  through my web brower's console");
+
+// Question # 06 : Make use of alerts in your new/existing HTML & CSS
+//project.
+
+var button = document.getElementById('submitButton');
+button.onclick = function() {
+    alert("Thank you for your submission!");
+};
+
+
+   
